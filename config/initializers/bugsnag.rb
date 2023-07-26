94f78aa4fd806d12751586726f42dcfdbb842459
dikshaingale <dikshaingale252@gmail.com>
2023-07-26 21:12:38 +0530

    set up circuitverse

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,9 @@
+# Bugsnag.configure do |config|
+#   config.api_key = ENV["BUGSNAG_API_KEY"]
+#   config.notify_release_stages = ["production"]
+# end  
+
 Bugsnag.configure do |config|
-  config.api_key = ENV["BUGSNAG_API_KEY"]
-  config.notify_release_stages = ["production"]
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::ERROR
 end
